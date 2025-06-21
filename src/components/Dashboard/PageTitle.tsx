8cd3412008637bb8a656d1054f17cdca731801c5
SadiqNaizam <sadiq.naizam1234@gmail.com>
2025-06-21 20:09:30 +0530

    Files generated: PageTitle.tsx

---
+++ src/components/Dashboard/PageTitle.tsx
@@ -0,0 +1,13 @@
+"use client";
+
+import React from 'react';
+
+const PageTitle: React.FC = () => {
+  return (
+    <div className="col-span-full">
+      <h1 className="text-2xl font-semibold text-foreground">Dashboard User</h1>
+    </div>
+  );
+};
+
+export default PageTitle;
